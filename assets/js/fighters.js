5580f04b9edf4445083c5156f3949afcf5149584
Barry Sheppard <barrysheppard@gmail.com>
2022-08-12 01:54:21 +0100

    Update fighters.js

--- assets/js/fighters.js
+++ assets/js/fighters.js
@@ -114,8 +114,8 @@ drawMaxAltitude = function(value) {
 
 drawPointCost = function(value) {
     getContext().fillStyle = 'black';
-    getContext().textAlign = "left";
-    writeScaled(value, {x: 1600, y: 85});
+    getContext().textAlign = "middle";
+    writeScaled(value, {x: 1610, y: 85});
 }
 
 
