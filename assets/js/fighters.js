1aa3b07477d0e4de0f832280fa837e8997aa1dea
Barry Sheppard <barrysheppard@gmail.com>
2022-08-12 09:53:50 +0100

    Moved weapon positions

--- assets/js/fighters.js
+++ assets/js/fighters.js
@@ -448,16 +448,16 @@ render = function(fighterData) {
     
     if (fighterData.weapon1.enabled && fighterData.weapon2.enabled)
     {
-        drawWeapon(fighterData.weapon1, {x: 50, y: 750}); // Default was x:29, y:397
-        drawWeapon(fighterData.weapon2, {x: 50, y: 950}); // Default was x:29, y:564
+        drawWeapon(fighterData.weapon1, {x: 20, y: 800}); // Default was x:29, y:397
+        drawWeapon(fighterData.weapon2, {x: 20, y: 850}); // Default was x:29, y:564
     }
     else if (fighterData.weapon1.enabled)
     {
-        drawWeapon(fighterData.weapon1, {x: 50, y: 750}); // Default was x:29, y:463
+        drawWeapon(fighterData.weapon1, {x: 20, y: 800}); // Default was x:29, y:463
     }
     else if (fighterData.weapon2.enabled)
     {
-        drawWeapon(fighterData.weapon2, {x: 50, y: 750}); // Default was x:29, y:463
+        drawWeapon(fighterData.weapon2, {x: 20, y: 800}); // Default was x:29, y:463
     }
 }
 
